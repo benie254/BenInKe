6280b75ae97c7be28a2996432c6e0356b8735971
benie254 <davinci.monalissa@gmail.com>
2023-05-17 19:53:26 +0300

    add poems module template contents && logic

--- Benie/src/app/modules/poems/poems.component.ts
+++ Benie/src/app/modules/poems/poems.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as Notiflix from 'notiflix';
+import { PoetryService } from './services/poetry.service';
 
 @Component({
   selector: 'app-poems',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./poems.component.css']
 })
 export class PoemsComponent implements OnInit {
+  poems: any;
+  twoPoems: any;
+  pinned: any;
+  allPoems: any;
+  count = 0;
+  poemId = '';
 
-  constructor() { }
+  constructor(
+    private poetryService:PoetryService,
+  ) { }
 
   ngOnInit(): void {
+    this.getAllPoems();
+    this.pinnedPoem();
+    this.bg();
   }
-
+  getAllPoems(){
+    Notiflix.Loading.pulse('Retrieving...')
+    this.poetryService.getAllPoems().subscribe({
+      next: (res) => {
+        Notiflix.Loading.remove();
+        this.allPoems = res;
+        this.poems = res.slice(0,4);
+        this.twoPoems = res.slice(0,2);
+        this.count = parseInt(this.allPoems.length) - parseInt(this.poems.length);
+        this.bg();
+      }
+    })
+  }
+  bg(){
+    let s = (<HTMLDivElement>document.getElementById('sch'));
+    s.style.color = 'white';
+  }
+  pinnedPoem(){
+    Notiflix.Loading.pulse('Retrieving...')
+    this.poetryService.getPinnedPoems().subscribe({
+      next: (res) => {
+        this.pinned = res;
+        Notiflix.Loading.remove();
+      }
+    })
+  }
 }
