502ad362f059cb730184a033807d7b620d899a38
benie254 <davinci.monalissa@gmail.com>
2023-05-17 11:32:05 +0300

    update contact module imports

--- Benie/src/app/modules/contact/contact.module.ts
+++ Benie/src/app/modules/contact/contact.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ContactRoutingModule } from './contact-routing.module';
-import { ContactComponent } from './contact.component';
 import { SubscribeComponent } from './subscribe/subscribe.component';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
-    ContactComponent,
     SubscribeComponent
   ],
   imports: [
     CommonModule,
-    ContactRoutingModule
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class ContactModule { }
