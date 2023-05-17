2be8d99d1c792562724f106a4241b11ff3a8fffc
benie254 <davinci.monalissa@gmail.com>
2023-05-17 11:37:27 +0300

    update discussions module imports

--- Benie/src/app/modules/discussions/discussions.module.ts
+++ Benie/src/app/modules/discussions/discussions.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { DiscussionsRoutingModule } from './discussions-routing.module';
 import { DiscussionsComponent } from './discussions.component';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { StoriesModule } from '../stories/stories.module';
 
 
 @NgModule({
@@ -11,7 +16,12 @@ import { DiscussionsComponent } from './discussions.component';
   ],
   imports: [
     CommonModule,
-    DiscussionsRoutingModule
+    DiscussionsRoutingModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    StoriesModule
   ]
 })
 export class DiscussionsModule { }
