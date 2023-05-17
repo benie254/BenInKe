e5e9d78f0a9369a0d21640d9e09b3ac38b791c84
benie254 <davinci.monalissa@gmail.com>
2023-05-17 20:21:27 +0300

    add app routes

--- Benie/src/app/app-routing.module.ts
+++ Benie/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { UnsubscribeComponent } from './components/unsubscribe/unsubscribe.component';
 
-const routes: Routes = [{ path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) }, { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) }, { path: 'contact', loadChildren: () => import('./modules/contact/contact.module').then(m => m.ContactModule) }, { path: 'discussions', loadChildren: () => import('./modules/discussions/discussions.module').then(m => m.DiscussionsModule) }, { path: 'navigation', loadChildren: () => import('./modules/navigation/navigation.module').then(m => m.NavigationModule) }, { path: 'poems', loadChildren: () => import('./modules/poems/poems.module').then(m => m.PoemsModule) }, { path: 'stories', loadChildren: () => import('./modules/stories/stories.module').then(m => m.StoriesModule) }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'unsubscribe', component: UnsubscribeComponent },
+  { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) }, 
+  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) }, 
+  { path: 'contact', loadChildren: () => import('./modules/contact/contact.module').then(m => m.ContactModule) }, 
+  { path: 'discussions', loadChildren: () => import('./modules/discussions/discussions.module').then(m => m.DiscussionsModule) }, 
+  { path: 'navigation', loadChildren: () => import('./modules/navigation/navigation.module').then(m => m.NavigationModule) }, 
+  { path: 'poems', loadChildren: () => import('./modules/poems/poems.module').then(m => m.PoemsModule) }, 
+  { path: 'stories', loadChildren: () => import('./modules/stories/stories.module').then(m => m.StoriesModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
