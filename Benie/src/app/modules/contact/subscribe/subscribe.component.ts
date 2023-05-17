45c20028eccdeabe1f8a5a5e9b203a962072e635
benie254 <davinci.monalissa@gmail.com>
2023-05-17 11:30:38 +0300

    update subscribe template && ts files

--- Benie/src/app/modules/contact/subscribe/subscribe.component.ts
+++ Benie/src/app/modules/contact/subscribe/subscribe.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import * as Notiflix from 'notiflix';
+import { Subscriber } from 'src/app/classes/subscriber/subscriber';
+import { StoryService } from 'src/app/modules/admin/services/story/story.service';
+import { MyErrorStateMatcher } from '../../admin/auth/services/matcher/matcher.service';
 
 @Component({
   selector: 'app-subscribe',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subscribe.component.css']
 })
 export class SubscribeComponent implements OnInit {
+  matcher = new MyErrorStateMatcher();
+  subValues = '';
+  subInput: boolean = false;
+  subImg = 'https://res.cloudinary.com/benie/image/upload/v1668273866/Humaaans_-_Space_xjvahn.png';
 
-  constructor() { }
+  constructor(
+    private storyService: StoryService,
+  ) { }
 
   ngOnInit(): void {
   }
-
+  subKey(event: any){
+    this.subValues = event.target.value;
+    if(this.subValues){
+      this.subInput = true;
+    }
+  }
+  subscribe(data: Subscriber){
+    Notiflix.Loading.pulse('subscribing you...')
+    this.storyService.addSub(data).subscribe({
+      next: (res) => {
+        Notiflix.Loading.remove();
+        this.subInput = false;
+        Notiflix.Report.success(
+          "You're In!",
+          'Your subscription was successful. Please check your email for more information.',
+          'Okay',
+        )
+      }
+    })
+  }
 }
