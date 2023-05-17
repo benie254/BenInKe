cdb7d30dcfc4d1ec7abc088aa27d707acb72cd18
benie254 <davinci.monalissa@gmail.com>
2023-05-17 20:15:22 +0300

    add stories module template contents & logic

--- Benie/src/app/modules/stories/stories.component.ts
+++ Benie/src/app/modules/stories/stories.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import * as Notiflix from 'notiflix';
+import { MyStoryService } from 'src/app/services/story/my-story.service';
 
 @Component({
   selector: 'app-stories',
@@ -6,10 +9,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stories.component.css']
 })
 export class StoriesComponent implements OnInit {
+  notifs: any = 6;
+  searchResults: any;
+  searchText: any;
+  title: any;
+  ongoing: any;
+  completed: any;
+  topOn: any;
+  topCom: any;
+  
 
-  constructor() { }
+  constructor(
+    private service: MyStoryService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    // this.checkViews();
+    this.allStories();
+    this.ongoingStories();
+    this.completedStories();
   }
-
+  reload(){
+    setTimeout(() => {
+      location.reload();
+    },3)
+  }
+  allStories(){
+    Notiflix.Loading.pulse('Retrieving...')
+    this.service.getAllStories().subscribe({
+      next: (res) => {
+        Notiflix.Loading.remove();
+        this.searchResults = res;
+      }
+    })
+  }
+  ongoingStories(){
+    this.service.getOngoingStories().subscribe({
+      next: (res) => {
+        this.ongoing = res;
+        this.topOn = res.slice(0,2);
+      }
+    })
+  }
+  completedStories(){
+    this.service.getCompletedStories().subscribe({
+      next: (res) => {
+        this.completed = res;
+        this.topCom = res.slice(0,2);
+      }
+    })
+  }
+  titleValue = (text: any): void => {
+    this.title = text;
+  }
+  checkViews(){
+    let hasVisited = sessionStorage.getItem('washere');
+    if ( ! hasVisited ) {
+    Notiflix.Notify.success('Welcome to Benie Writes!');
+    sessionStorage.setItem('washere', true.toString());
+    } else {
+      this.notifs = '';
+      sessionStorage.removeItem('washere')
+    }
+  }
 }
