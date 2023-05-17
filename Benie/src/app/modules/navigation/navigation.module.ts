f9382161fd434e5ec39bb5bf3da3565a904cabe4
benie254 <davinci.monalissa@gmail.com>
2023-05-17 11:40:26 +0300

    remove unused imports of navigation module

--- Benie/src/app/modules/navigation/navigation.module.ts
+++ Benie/src/app/modules/navigation/navigation.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { NavigationRoutingModule } from './navigation-routing.module';
-import { NavigationComponent } from './navigation.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    NavigationComponent,
     FooterComponent,
     NavbarComponent
   ],
   imports: [
     CommonModule,
-    NavigationRoutingModule
   ]
 })
 export class NavigationModule { }
