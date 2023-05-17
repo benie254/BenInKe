f85471efef1b2ff9f027665f632178af6491fb2f
benie254 <davinci.monalissa@gmail.com>
2023-05-17 20:14:15 +0300

    add stories module routes

--- Benie/src/app/modules/stories/stories-routing.module.ts
+++ Benie/src/app/modules/stories/stories-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StoriesComponent } from './stories.component';
+import { ReadComponent } from './components/read/read.component';
+import { ResultsComponent } from './components/results/results.component';
+import { StoryPagesComponent } from './components/story-pages/story-pages.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: '', component: StoriesComponent }];
+const routes: Routes = [
+  { path: '', component: StoriesComponent },
+  { path: 'library', component: HomeComponent },
+  { path: ':id/:id/preview/:id', component: ReadComponent },
+  { path: ':id/:id/chapter/:id', component: StoryPagesComponent },
+  { path: ':id/:id/read/:id', component: StoryPagesComponent },
+  { path: 'related/:id/:id', component: ResultsComponent },];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
