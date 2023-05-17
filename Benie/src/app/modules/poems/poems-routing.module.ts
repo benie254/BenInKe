6399bdf9d0eb47cf21c5de570844b3d10eb0cb90
benie254 <davinci.monalissa@gmail.com>
2023-05-17 19:52:33 +0300

    add poems module routes

--- Benie/src/app/modules/poems/poems-routing.module.ts
+++ Benie/src/app/modules/poems/poems-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PoemsComponent } from './poems.component';
+import { AllPoemsComponent } from './components/all-poems/all-poems.component';
+import { ReadComponent } from './components/read/read.component';
+import { ResultsComponent } from './components/results/results.component';
 
-const routes: Routes = [{ path: '', component: PoemsComponent }];
+const routes: Routes = [
+  { path: '', component: PoemsComponent },
+  { path: ':id/:id/read/:id', component: ReadComponent },
+  { path: 'all', component: AllPoemsComponent },
+  { path: 'related/category/:id', component: ResultsComponent },
+  { path: 'related/date/:id', component: ResultsComponent },];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
