37fad6ca9fd080d952a06830456b7ef33c5e1a1f
benie254 <davinci.monalissa@gmail.com>
2023-05-17 20:04:28 +0300

    add notifications template contents & logic

--- Benie/src/app/modules/stories/components/notifications/notifications.component.ts
+++ Benie/src/app/modules/stories/components/notifications/notifications.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as Notiflix from 'notiflix';
+import { MyStoryService } from 'src/app/services/story/my-story.service';
 
 @Component({
   selector: 'app-notifications',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications.component.css']
 })
 export class NotificationsComponent implements OnInit {
+  notifs: any;
+  latestNotifs: any;
+  panelOpenState = false;
+  today = new Date();
+  authorImg = 'https://res.cloudinary.com/benie/image/upload/v1667972682/h02js8etvetdr5ctbmtf.jpg';
 
-  constructor() { }
+  constructor(
+    private service: MyStoryService,
+  ) { }
 
   ngOnInit(): void {
+    this.getNotifications();
   }
-
+  getNotifications(){
+    Notiflix.Loading.pulse('fetching...')
+    this.service.getAllNotifications().subscribe({
+      next: (res) => {
+        Notiflix.Loading.remove();
+        this.notifs = res;
+        this.latestNotifs = this.notifs.slice(0,2)
+      }
+    })
+  }
 }
