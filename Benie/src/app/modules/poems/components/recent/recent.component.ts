341260cb609b04b82a98548d441fcad561debe4c
benie254 <davinci.monalissa@gmail.com>
2023-05-17 19:46:18 +0300

    add recent template contents & logic

--- Benie/src/app/modules/poems/components/recent/recent.component.ts
+++ Benie/src/app/modules/poems/components/recent/recent.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import * as Notiflix from 'notiflix';
 
 @Component({
   selector: 'app-recent',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recent.component.css']
 })
 export class RecentComponent implements OnInit {
+  @Input() poems: any;
+  poemId: any;
+  poemT: any;
+  poemC: any;
 
-  constructor() { }
+  constructor(
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
+  }
+  copy(text: any){
+    localStorage.setItem("poemId",text);
+    this.poemId = localStorage.getItem("poemId");
+    console.warn("id",this.poemId)
+  }
+  copyT(text: any){
+    localStorage.setItem("poemTitle",text);
+    this.poemT = localStorage.getItem("poemTitle");
+  }
+  copyC(text: any){
+    localStorage.setItem("poemCateg",text);
+    this.poemC = localStorage.getItem("poemCateg");
+  }
+  refresh(){
+    this.router.navigate(['/poems/' + this.poemC + '/' + this.poemT + '/' + 'read' + this.poemId])
+    setTimeout(() => {
+      location.reload();
+      localStorage.removeItem("poemId");
+      localStorage.removeItem("poemTitle");
+      localStorage.removeItem("poemCateg");
+    },2)
   }
 
 }
