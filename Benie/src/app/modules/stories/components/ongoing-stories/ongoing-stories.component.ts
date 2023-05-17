67034c6af21a5f148d6ea20fd47d5e784a693045
benie254 <davinci.monalissa@gmail.com>
2023-05-17 20:05:04 +0300

    add ongoing stories template contents & logic

--- Benie/src/app/modules/stories/components/ongoing-stories/ongoing-stories.component.ts
+++ Benie/src/app/modules/stories/components/ongoing-stories/ongoing-stories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as Notiflix from 'notiflix';
+import { MyStoryService } from 'src/app/services/story/my-story.service';
 
 @Component({
   selector: 'app-ongoing-stories',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ongoing-stories.component.css']
 })
 export class OngoingStoriesComponent implements OnInit {
+  onStories: any;
+  page: number = 1;
+  count: number = 0;
+  tableSize: number = 4;
+  tableSizes: any = [2, 5, 10, 15];
 
-  constructor() { }
+  constructor(
+    private service: MyStoryService,
+  ) { }
 
   ngOnInit(): void {
+    this.ongoingStories()
+  }
+  ongoingStories(){
+    Notiflix.Loading.pulse('fetching stories...')
+    this.service.getOngoingStories().subscribe({
+      next: (data) => {
+        this.onStories = data;
+        Notiflix.Loading.remove();
+      }
+    })
+  }
+  onTableDataChange = (event: any): void => {
+    this.page = event;
+    this.ongoingStories();
+  }
+  onTableSizeChange(event: any): void {
+    this.tableSize = event.target.value;
+    this.page = 1;
+    this.ongoingStories();
   }
 
 }
