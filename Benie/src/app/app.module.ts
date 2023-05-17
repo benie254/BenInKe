0e2c169890f19381ec7a62df64100b1ea0d681cb
benie254 <davinci.monalissa@gmail.com>
2023-05-17 20:24:51 +0300

    update app's imports

--- Benie/src/app/app.module.ts
+++ Benie/src/app/app.module.ts
@@ -7,6 +7,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
 import { UnsubscribeComponent } from './components/unsubscribe/unsubscribe.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatListModule} from '@angular/material/list';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { StoriesModule } from './modules/stories/stories.module';
+import { NavigationModule } from './modules/navigation/navigation.module';
+import { ContactModule } from './modules/contact/contact.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +38,30 @@ import { UnsubscribeComponent } from './components/unsubscribe/unsubscribe.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatListModule,
+    MatBottomSheetModule,
+    MatInputModule,
+    NgxPaginationModule,
+    ReactiveFormsModule,
+    MatBadgeModule,
+    MatExpansionModule,
+    Ng2SearchPipeModule,
+    NgxEditorModule,
+    MatChipsModule,
+    StoriesModule,
+    NavigationModule,
+    ContactModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
