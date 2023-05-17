053d056d34483ab2b1705e4ea6bb8a9f89fcaee2
benie254 <davinci.monalissa@gmail.com>
2023-05-17 20:02:29 +0300

    add completed stories template contents & logic

--- Benie/src/app/modules/stories/components/completed-stories/completed-stories.component.ts
+++ Benie/src/app/modules/stories/components/completed-stories/completed-stories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as Notiflix from 'notiflix';
+import { MyStoryService } from 'src/app/services/story/my-story.service';
 
 @Component({
   selector: 'app-completed-stories',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./completed-stories.component.css']
 })
 export class CompletedStoriesComponent implements OnInit {
+  compStories: any;
+  page: number = 1;
+  count: number = 0;
+  tableSize: number = 4;
+  tableSizes: any = [2, 5, 10, 15];
 
-  constructor() { }
+  constructor(
+    private service: MyStoryService,
+  ) { }
 
   ngOnInit(): void {
+    this.completedStories()
+  }
+  completedStories(){
+    Notiflix.Loading.pulse('fetching stories...')
+    this.service.getCompletedStories().subscribe({
+      next: (data) => {
+        this.compStories = data;
+        Notiflix.Loading.remove();
+      }
+    })
+  }
+  onTableDataChange = (event: any): void => {
+    this.page = event;
+    this.completedStories();
+  }
+  onTableSizeChange(event: any): void {
+    this.tableSize = event.target.value;
+    this.page = 1;
+    this.completedStories();
   }
 
 }
