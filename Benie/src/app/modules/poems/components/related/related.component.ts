36dfacfb46ef86c878c6f26c468fa10d67b38bb6
benie254 <davinci.monalissa@gmail.com>
2023-05-17 19:47:21 +0300

    add related template contents & logic

--- Benie/src/app/modules/poems/components/related/related.component.ts
+++ Benie/src/app/modules/poems/components/related/related.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-related',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./related.component.css']
 })
 export class RelatedComponent implements OnInit {
+  @Input() topRelated: any;
+  poemId: any;
+  poemT: any;
+  poemC: any;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
+  copy(text: any){
+    localStorage.setItem("poemId",text);
+    this.poemId = localStorage.getItem("poemId");
+  }
+  copyT(text: any){
+    localStorage.setItem("poemTitle",text);
+    this.poemT = localStorage.getItem("poemTitle");
+  }
+  copyC(text: any){
+    localStorage.setItem("poemCateg",text);
+    this.poemC = localStorage.getItem("poemCateg");
+  }
+  refresh(){
+    this.router.navigate(['/poems/' + this.poemC + '/' + this.poemT + '/' + 'read' + this.poemId])
+    setTimeout(() => {
+      location.reload();
+      localStorage.removeItem("poemId");
+      localStorage.removeItem("poemTitle");
+      localStorage.removeItem("poemCateg");
+    },2)
+  }
+
 
 }
