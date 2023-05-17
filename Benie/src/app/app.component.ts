950ba35cd5e2007aed888677c44dbdba9830bf2c
benie254 <davinci.monalissa@gmail.com>
2023-05-17 20:22:16 +0300

    update root component & logic

--- Benie/src/app/app.component.ts
+++ Benie/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+import { Cloudinary, CloudinaryImage } from '@cloudinary/url-gen';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Benie';
+  title = 'Benie Writes';
+  img!: CloudinaryImage;
+
+  constructor(
+    private meta: Meta,
+  ){
+
+  }
+
+  ngOnInit(){
+    this.meta.updateTag({name: 'title', content: 'Benie Writes'}) 
+    this.meta.updateTag({name: 'description', content: 'A home for stories, poems, discussions'}) 
+    this.meta.updateTag({name: 'image', content: 'https://res.cloudinary.com/benie/image/upload/v1671558026/IMG_20220119_175258_460-modified_yhqkhm.png'}) 
+    this.meta.updateTag({name: 'site', content: 'https://benie.web.app'}) 
+    
+  }
 }
